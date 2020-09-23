01dee87acd73be356ea1a439c546746900f4a103
Jeff Ochoa <jeffer.8a@gmail.com>
2020-09-23 08:42:48 -0300

    fix response status in the view

--- resources/views/block/request-info.blade.php
+++ resources/views/block/request-info.blade.php
@@ -1,7 +1,7 @@
 <div class="my-2">
     <x-enlighten-sub-title>Request</x-enlighten-sub-title>
     <p class="py-4 border-b border-gray-500 mb-4">
-        <span class="p-1 bg-blue-200 text-blue-500">{{ $codeExample->requestMethod }}</span>
+        <span class="p-1 bg-blue-200 text-blue-500">{{ $codeExample->request_method }}</span>
         <span class="text-gray-200">{{ $codeExample->route }}</span>
     </p>
 
