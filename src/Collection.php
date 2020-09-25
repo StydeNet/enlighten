416328ec38b16dd20fe2baa686035063edcf3e06
Duilio Palacios <duilio@styde.net>
2020-09-25 10:46:15 +0100

    fix docblock

--- src/Collection.php
+++ src/Collection.php
@@ -6,6 +6,9 @@ class Collection extends \Illuminate\Support\Collection
 {
     /**
      * Return a collection without the given keys.
+     *
+     * @param Collection $keys
+     * @return Collection
      */
     public function exclude(Collection $keys): Collection
     {
@@ -14,6 +17,9 @@ class Collection extends \Illuminate\Support\Collection
 
     /**
      * Overwrite the values that are already present in the collection with the given values.
+     *
+     * @param Collection $values
+     * @return Collection
      */
     public function overwrite(Collection $values): Collection
     {
