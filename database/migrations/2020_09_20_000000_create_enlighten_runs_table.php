0a6ec09e40260bc2612aa47512a154a5a807659b
Duilio Palacios <duilio@styde.net>
2020-10-04 12:03:59 +0100

    modify existing migrations and add new runs table

---
+++ database/migrations/2020_09_20_000000_create_enlighten_runs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnlightenTestRunsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::connection('enlighten')->hasTable('enlighten_runs')) {
+            return;
+        }
+
+        Schema::connection('enlighten')->create('enlighten_runs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('branch');
+            $table->string('head')->unique();
+            $table->boolean('modified');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('enlighten')->dropIfExists('enlighten_example_runs');
+    }
+}
