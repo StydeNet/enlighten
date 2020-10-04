e42503d748787aebff4c02468f3287ff40c849dc
Duilio Palacios <duilio@styde.net>
2020-10-04 12:09:14 +0100

    fix migrations name

--- database/migrations/2020_09_20_000000_create_enlighten_runs_table.php
+++ database/migrations/2020_09_20_000000_create_enlighten_runs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEnlightenTestRunsTable extends Migration
+class CreateEnlightenRunsTable extends Migration
 {
     /**
      * Run the migrations.
