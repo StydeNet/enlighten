4dc4323a30a4695ae9e1bf96ce81f35f590d3976
Jeff Ochoa <jeffer.8a@gmail.com>
2020-10-06 07:29:42 -0300

    fixing icons alignment on the modul-panel section

--- resources/views/dashboard/_module-panel.blade.php
+++ resources/views/dashboard/_module-panel.blade.php
@@ -12,7 +12,8 @@
     <ul class="py-4">
         @foreach($module->groups as $group)
             <li>
-                <a href="{{ route('enlighten.group.show', ['suite' => $suite->slug, 'run' => request()->route('run'), 'group' => $group]) }}" class="block py-2 px-4 text-gray-700 hover:text-teal-500 hover:bg-gray-100 transition-all ease-in-out duration-100">
+                <a href="{{ route('enlighten.group.show', ['suite' => $suite->slug, 'run' => request()->route('run'), 'group' => $group]) }}"
+                   class="flex items-center py-2 px-4 text-gray-700 hover:text-teal-500 hover:bg-gray-100 transition-all ease-in-out duration-100">
                     <x-enlighten-status-badge size="6" :model="$group" /> {{ $group->title }}
                 </a>
             </li>
