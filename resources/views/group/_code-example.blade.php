64906547ecfb78f7b2483f352f46bc80bef381cc
Jeff Ochoa <jeffer.8a@gmail.com>
2020-10-12 09:06:19 -0300

    preview database queries

--- resources/views/group/_code-example.blade.php
+++ resources/views/group/_code-example.blade.php
@@ -30,6 +30,14 @@
                         <x-enlighten-request-info :http-data="$http_data" />
                         <span class="mb-8 w-full block"></span>
                         <x-enlighten-response-info :http-data="$http_data" />
+                        <span class="mb-8 w-full block"></span>
+                        <x-enlighten-info-panel>
+                            <x-slot name="title">Database Queries</x-slot>
+                            @foreach($example->queries as $query)
+                                <x-enlighten-pre language="sql" :code="$query->sql"></x-enlighten-pre>
+                                <x-enlighten-key-value :items="$query->bindings"></x-enlighten-key-value>
+                            @endforeach
+                        </x-enlighten-info-panel>
                     </div>
                     <x-enlighten-response-preview :http-data="$http_data"/>
                 </div>
