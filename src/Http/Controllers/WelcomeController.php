ff8a0f8e475015dccd24b016d31a11d8bdc2e24d
Duilio Palacios <duilio@styde.net>
2020-10-16 16:29:55 +0100

    fix route to preview image. Use single action controller and extract helper methods

--- src/Http/Controllers/WelcomeController.php
+++ src/Http/Controllers/WelcomeController.php
@@ -6,17 +6,32 @@ use Illuminate\Mail\Markdown;
 
 class WelcomeController extends Controller
 {
-    public function intro()
+    public function __invoke()
     {
-        if (file_exists(base_path('ENLIGHTEN.md'))) {
-            $content = Markdown::parse(base_path('ENLIGHTEN.md'));
-        } else {
-            $content = Markdown::parse(file_get_contents(__DIR__ . '/../../../README.md'));
-        }
-
         return view('enlighten::intro', [
-            'content' => $content,
+            'content' => $this->getIntroContent(),
             'tabs' => $this->getTabs()
         ]);
+    }
+
+    private function getIntroContent()
+    {
+        if (file_exists(base_path('ENLIGHTEN.md'))) {
+            return $this->parseMarkdownFile(base_path('ENLIGHTEN.md'));
+        } else {
+            return $this->fixImagesPath($this->parseMarkdownFile(__DIR__ . '/../../../README.md'));
+        }
+    }
+
+    private function parseMarkdownFile(string $filePath)
+    {
+        return Markdown::parse(file_get_contents($filePath));
+    }
+
+    private function fixImagesPath(string $content)
+    {
+        $baseImagePath = asset('vendor/enlighten/img') . '/';
+
+        return str_replace('<img src="./', '<img src="'.$baseImagePath, $content);
     }
 }
