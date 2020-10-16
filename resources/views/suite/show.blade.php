dc9324dc5c653c328337c72b6b17d063361c1aa0
Duilio Palacios <duilio@styde.net>
2020-10-16 11:35:51 +0100

    Add area name and message if area is empty

--- resources/views/suite/show.blade.php
+++ resources/views/suite/show.blade.php
@@ -1,9 +1,15 @@
 <x-enlighten-main-layout>
+    <x-slot name="title">{{ $suite->title }} Area</x-slot>
+
     <div class="w-full mx-auto">
         <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
-            @foreach($modules as $module)
+            @forelse($modules as $module)
                 <x-enlighten-module-panel :suite="$suite" :module="$module"></x-enlighten-module-panel>
-            @endforeach
+            @empty
+                <p class="text-white">
+                    There are no examples in the "{{ $suite->title }}" area.
+                </p>
+            @endforelse
         </div>
     </div>
 </x-enlighten-main-layout>
