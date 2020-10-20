472dad08a56300ee4262025399fcdc057d4e448f
Jeff Ochoa <jeffer.8a@gmail.com>
2020-10-20 07:59:40 -0300

    display test methos as a list

--- resources/views/group/show.blade.php
+++ resources/views/group/show.blade.php
@@ -1,21 +1,60 @@
 <x-enlighten-main-layout>
-    <div class="w-full">
-        @if($group->description)
-            <p class="text-gray-100 mb-8 text-lg">
-                {{ $group->description }}
-            </p>
-        @endif
 
-        <div class="flex space-x-4">
-            <div class="2/12">
-                <x-enlighten-content-table :examples="$group->examples"></x-enlighten-content-table>
-            </div>
-            <div class="flex-1">
-                @foreach($group->examples as $example)
-                    <x-enlighten-code-example :example="$example"/>
+    <x-slot name="title">{{ $title }}</x-slot>
+
+    <div class="w-full my-4">
+            <div class="w-full divide-y divide-gray-300 bg-white rounded-lg overflow-hidden">
+                @foreach ($group->examples as $example)
+                    <a href="{{ $example->url }}" class="flex justify-between items-center p-4 hover:bg-gray-100">
+
+                        <div> <!-- left-->
+                            <div class="flex space-x-2 items-center">
+                                <span aria-label="Running" class="h-4 w-4 bg-{{ $example->status }}-100 rounded-full flex items-center justify-center">
+                                    <span class="h-2 w-2 bg-{{ $example->status }}-400 rounded-full"></span>
+                                </span>
+                                <span class="text-gray-700 font-medium">{{ $example->title }}</span>
+                            </div>
+                            <div class="flex space-x-4 mt-4">
+                                @if($example->exception)
+                                    <span class="flex space-x-2 items-center text-gray-500">
+                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    </span>
+                                @endif
+                                @if($example->queries)
+                                    <span class="flex space-x-1 items-center text-gray-500">
+                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"></path></svg>
+                                        <span>{{ $example->queries->count() }}</span>
+                                    </span>
+                                @endif
+                                @if($example->http_data)
+                                    <span class="flex space-x-2 items-center text-gray-500">
+                                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 3h5m0 0v5m0-5l-6 6M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"></path></svg>
+                                        <span>{{ $example->http_data->count() }}</span>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="flex-1 flex justify-end"> <!-- right-->
+                            @if($example->http_data)
+                                <div class="space-y-4">
+                                    <div class="flex justify-end">
+                                        <span class="text-sm px-2 py-1 rounded-full text-gray-700 bg-gray-300">
+                                                {{ $example->http_data->first()->request_method }}
+                                        </span>
+                                    </div>
+                                    <span class="block text-gray-800">
+                                        /{{ $example->http_data->first()->request_path }}
+                                    </span>
+                                </div>
+                            @endif
+                        </div>
+
+                        <div class="flex items-center justify-center flex-shrink pl-8">
+                            <svg class="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                        </div>
+                    </a>
                 @endforeach
             </div>
-        </div>
-        <x-enlighten-scroll-to-top/>
     </div>
 </x-enlighten-main-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   future: {
     // removeDeprecatedGapUtilities: true,
@@ -11,6 +13,33 @@ module.exports = {
     // ]
   },
   theme: {
+    colors: {
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        teal: {
+            '100': '#D2F9F7',
+            '200': '#A6F2F0',
+            '300': '#79ECE8',
+            '400': '#4CE6E0',
+            '500': '#20DFD9',
+            '600': '#19AEA9',
+            '700': '#127D79',
+            '800': '#0B4C4A',
+            '900': '#041B1A'
+        },
+        red: colors.red,
+        yellow: colors.yellow,
+        green: colors.green,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        purple: colors.purple,
+        orange: colors.orange,
+        pink: colors.pink,
+        success: colors.green,
+        warning: colors.yellow,
+        failure: colors.red
+    },
     maxHeight: {
       '120': '30rem'
     },
