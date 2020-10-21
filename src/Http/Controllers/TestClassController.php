d49dd5e8bf81fc2ae0a747ca53eb689e5b6fe018
MostafaRabia <mostafarabia64@gmail.com>
2020-10-21 18:35:26 +0200

    simple solve, but not best
    I know it's not best thing, but it works for now.

--- src/Http/Controllers/TestClassController.php
+++ src/Http/Controllers/TestClassController.php
@@ -7,8 +7,12 @@ use Styde\Enlighten\Models\Run;
 
 class TestClassController extends Controller
 {
-    public function show(Run $run, string $area, ExampleGroup $group)
+    public function show(Run $run, string $area, $group)
     {
+        $tabs = $this->getTabs();
+        $area = $tabs->firstWhere('slug', $area);
+
+        $group = ExampleGroup::filterByArea($area)->where('slug',$group)->first();
         $group->load(['examples', 'examples.http_data']);
 
         return view('enlighten::group.show', [
