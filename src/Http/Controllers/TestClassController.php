28c548978117812a607397868608638af46a12cf
Duilio Palacios <duilio@styde.net>
2020-10-21 21:05:42 +0100

    Avoid ambiguity when 2 example groups share the same class name / slug

--- src/Http/Controllers/TestClassController.php
+++ src/Http/Controllers/TestClassController.php
@@ -7,8 +7,13 @@ use Styde\Enlighten\Models\Run;
 
 class TestClassController extends Controller
 {
-    public function show(Run $run, string $area, ExampleGroup $group)
+    public function show(Run $run, string $area, string $group)
     {
+        $group = ExampleGroup::where([
+            'area' => $area,
+            'slug' => $group,
+        ])->firstOrFail();
+
         $group->load(['examples', 'examples.http_data']);
 
         return view('enlighten::group.show', [
