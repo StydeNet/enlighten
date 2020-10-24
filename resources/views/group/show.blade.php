d60573585cc62a5e90072741b07d48095d8c36a4
Jeff Ochoa <jeffer.8a@gmail.com>
2020-10-24 09:32:15 -0300

    Show queries count if > 0

--- resources/views/group/show.blade.php
+++ resources/views/group/show.blade.php
@@ -43,7 +43,7 @@
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                 </span>
                             @endif
-                            @if($example->queries)
+                            @if($example->queries->isNotEmpty())
                                 <span class="flex space-x-1 items-center text-gray-600">
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"></path></svg>
                                     <span>{{ $example->queries->count() }}</span>
