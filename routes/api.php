9d932febf8b49fa640dfce48c3f556bdd0cfbfd4
Jeff Ochoa <jeffer.8a@gmail.com>
2020-10-25 12:37:25 -0300

    removing -widgets- logic

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,3 @@ use Styde\Enlighten\Http\Controllers\SearchController;
 Route::prefix('enlighten/api')->group(function () {
     Route::get('run/{run}/search', [SearchController::class, 'index'])->name('enlighten.api.search');
 });
-
-Route::prefix('/enlighten/widget')->group(function () {
-    Route::get('/{widget}', WidgetController::class)->name('enlighten.widget');
-});
-
-
-// enlighten/runs-table
