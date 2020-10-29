fdd17732dddcb0adbd1fb3228f945e1e92fc2336
asilvabe <asilvabe24@gmail.com>
2020-10-29 11:19:50 -0500

    use only simple quotes

--- resources/views/components/queries-info.blade.php
+++ resources/views/components/queries-info.blade.php
@@ -13,7 +13,7 @@
             <div class="flex flex-col space-y-4">
                 @foreach($group as $query)
                     <x-enlighten-info-panel>
-                        <x-slot name="title">{{ __("Time:") }} {{ $query->time }}</x-slot>
+                        <x-slot name="title">{{ __('Time:') }} {{ $query->time }}</x-slot>
                         <x-enlighten-pre language="sql" :code="$query->sql"></x-enlighten-pre>
                         @if($query->bindings)
                             <x-enlighten-key-value :items="$query->bindings" title="Bindings"></x-enlighten-key-value>
@@ -23,12 +23,12 @@
             </div>
         </div>
     @else
-        <h2 class="text-gray-300 mb-6 mt-2 text-xl">{{ __("Request Queries") }}</h2>
+        <h2 class="text-gray-300 mb-6 mt-2 text-xl">{{ __('Request Queries') }}</h2>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
             <div class="flex flex-col md:col-span-2 space-y-4">
                 @foreach($group as $query)
                     <x-enlighten-info-panel>
-                        <x-slot name="title">{{ __("Time:") }} {{ $query->time }}</x-slot>
+                        <x-slot name="title">{{ __('Time:') }} {{ $query->time }}</x-slot>
                         <x-enlighten-pre language="sql" :code="$query->sql"></x-enlighten-pre>
                         @if($query->bindings)
                             <x-enlighten-key-value :items="$query->bindings" title="Bindings"></x-enlighten-key-value>
