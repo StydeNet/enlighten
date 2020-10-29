d3335d3da118b16066c10392223969e181afdf23
asilvabe <asilvabe24@gmail.com>
2020-10-29 12:39:04 -0500

    define lang files as php arrays

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'Branch / Commit'=> 'Rama / Confirmación',
+    'Dashboard'=> 'Tablero',
+    'Date'=> 'Fecha',
+    'Features'=> 'Funcionalidades',
+    'Input'=> 'Entrada',
+    'Output'=> 'Salida',
+    'Pattern'=> 'Patrón',
+    'Request Queries'=> 'Consultas de peticiones',
+    'Requirement'=> 'Requerimiento',
+    'Response'=> 'Respuesta',
+    'Route Parameter'=> 'Parámetro de ruta',
+    'Session data'=> 'Datos de sesión',
+    'Snippet'=> 'Snippet',
+    'Stats'=> 'Estadísticas',
+    'Test Queries'=> 'Consultas de prueba',
+    'There are no examples to show'=> 'No hay ejemplos para mostrar.',
+    'Time'=> 'Tiempo',
+    'Value'=> 'Valor',
+    'View'=> 'Vista'
+];
