7b1bd1687f2a8ea07d41fa226d81f2048fed476f
jeffochoa <jeffochoa@users.noreply.github.com>
2020-11-01 13:44:06 +0000

    Apply php-cs-fixer changes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Styde\Enlighten\Http\Controllers\RunController;
 use Styde\Enlighten\Http\Controllers\WelcomeController;
 
 Route::prefix('enlighten')->middleware('web')->group(function () {
-
     Route::get('/intro', WelcomeController::class)
             ->name('enlighten.intro');
 
