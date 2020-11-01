0994ef5c9ef130217316975376aa55a2018bc6c1
Jeff Ochoa <jeffer.8a@gmail.com>
2020-11-01 17:06:30 -0300

    wip

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Styde\Enlighten\Http\Controllers\SearchController;
-use Styde\Enlighten\Http\Controllers\WidgetController;
 
 Route::prefix('enlighten/api')->group(function () {
     Route::get('run/{run}/search', SearchController::class)->name('enlighten.api.search');
