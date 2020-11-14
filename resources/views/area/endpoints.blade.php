eaef37f91d1e703a7f56be8aa93d905a35dcc47f
Jeff Ochoa <jeffer.8a@gmail.com>
2020-11-14 09:06:06 -0300

    wip

--- resources/views/area/endpoints.blade.php
+++ resources/views/area/endpoints.blade.php
@@ -10,8 +10,8 @@
                 @foreach($module->groups as $group)
                     <div class="w-full" x-data="{open: false}">
                         <div class="flex space-y-4 lg:space-y-0 justify-between p-2 md:p-4 hover:bg-gray-100"
-                             x-bind:class="{ 'bg-gray-200': open }">
-                            <div class="flex items-center w-full justify-between flex-col md:flex-row">
+                             x-bind:class="{'hover:bg-gray-300 bg-gray-300': open}">
+                            <a href="{{ $group->mainRequest->example->url }}" class="flex items-center w-full justify-between flex-col md:flex-row">
                                 <div class="flex items-center w-full md:w-auto">
                                     <div class="flex space-x-2 items-start">
                                         <x-enlighten-status-badge :model="$group->mainRequest->example" size="6"></x-enlighten-status-badge>
@@ -20,15 +20,15 @@
                                         <span class="px-2 flex items-center text-gray-700 bg-gray-200">
                                             {{ $group->method }}: {{ $group->route }}
                                         </span>
-                                        <a href="{{ $group->mainRequest->example->url }}" class="py-3 md:py-0 md:px-4 text-gray-700 hover:text-teal-400">
+                                        <div class="py-3 md:py-0 md:px-4 text-gray-700 hover:text-teal-400">
                                             {{ $group->mainRequest->example->title }}
-                                        </a>
+                                        </div>
                                     </div>
                                 </div>
                                 <span class="text-sm px-2 flex items-center text-{{ $group->mainRequest->getStatus() }}-700 bg-{{ $group->mainRequest->getStatus() }}-200">
                                     {{ $group->mainRequest->response_status }} {{ $group->mainRequest->response_type }}
                                 </span>
-                            </div>
+                            </a>
                             <div class="w-12 flex justify-end">
                                 @if($group->additionalRequests->isNotEmpty())
                                     <button type="button" x-on:click="open = !open" class="rounded-md focus:outline-none bg-gray-300 p-1">
@@ -38,29 +38,34 @@
                                 @endif
                             </div>
                         </div>
-                        @foreach ($group->additionalRequests as $additionalRequest)
-                            <div x-cloak x-show="open" class="flex {{ $loop->first ? 'border-t' : '' }} {{ $loop->last ? '' : 'border-b' }} flex-col space-x-2 space-y-4 lg:space-y-0 lg:flex-row justify-between p-2 md:p-4 hover:bg-gray-100 border-l-8 border-gray-300">
-                                <div class="flex items-center w-full md:w-auto">
-                                    <div class="flex space-x-2 items-start">
-                                        <x-enlighten-status-badge :model="$additionalRequest->example" size="6"></x-enlighten-status-badge>
+                        <div class="shadow-inner w-full">
+                            @foreach ($group->additionalRequests as $additionalRequest)
+                                <a href="{{ $additionalRequest->example->url }}"
+                                   x-cloak
+                                   x-show="open"
+                                   class="flex {{ $loop->first ? 'border-t' : '' }} {{ $loop->last ? '' : 'border-b' }} flex-col space-x-2 space-y-4 lg:space-y-0 lg:flex-row justify-between p-2 md:p-4 hover:bg-gray-100 bg-gray-200 border-gray-300">
+                                    <div class="flex items-center w-full md:w-auto">
+                                        <div class="flex space-x-2 items-start">
+                                            <x-enlighten-status-badge :model="$additionalRequest->example" size="6"></x-enlighten-status-badge>
+                                        </div>
+                                        <div class="flex space-x-2">
+                                            <span class="px-2 flex items-center text-gray-700 bg-gray-200">
+                                                {{ $additionalRequest->request_method }}: {{ $additionalRequest->route_or_path }}
+                                            </span>
+                                            <div class="text-gray-700 pl-2 w-full md:flex-1 hover:text-teal-400">
+                                                {{ $additionalRequest->example->title }}
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="flex space-x-2">
-                                        <span class="text-sm px-2 flex items-center text-gray-700 bg-gray-200">
-                                            {{ $additionalRequest->request_method }}: {{ $additionalRequest->route_or_path }}
+                                    <div class="flex">
+                                        <span class="text-sm px-2 flex items-center text-{{ $additionalRequest->getStatus() }}-700 bg-{{ $additionalRequest->getStatus() }}-200">
+                                            {{ $additionalRequest->response_status }} {{ $additionalRequest->response_type }}
                                         </span>
-                                        <a href="{{ $additionalRequest->example->url }}" class="text-gray-700 px-2 w-full md:flex-1 hover:text-teal-400">
-                                            {{ $additionalRequest->example->title }}
-                                        </a>
+                                        <span class="w-12"></span>
                                     </div>
-                                </div>
-                                <div class="flex">
-                                    <span class="text-sm px-2 flex items-center text-{{ $additionalRequest->getStatus() }}-700 bg-{{ $additionalRequest->getStatus() }}-200">
-                                        {{ $additionalRequest->response_status }} {{ $additionalRequest->response_type }}
-                                    </span>
-                                    <span class="w-12"></span>
-                                </div>
-                            </div>
-                        @endforeach
+                                </a>
+                            @endforeach
+                        </div>
                     </div>
                 @endforeach
             </div>
