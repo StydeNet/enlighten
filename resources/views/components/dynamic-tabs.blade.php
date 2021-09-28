17276ce06cf830f12e7c3161e389498fc51de987
emmannl <eul.nelson@gmail.com>
2021-09-28 17:27:49 +0100

    fix empty data on test method screen

--- resources/views/components/dynamic-tabs.blade.php
+++ resources/views/components/dynamic-tabs.blade.php
@@ -3,7 +3,7 @@
             <div class="block">
                 <div class="flex space-x-4 mb-4">
                     @foreach($tabs_collection as $name => $title)
-                        @if(isset($$name) && $$name instanceof \Illuminate\Support\HtmlString && !$$name->isEmpty())
+                        @if(isset($$name) && $$name instanceof $htmlable && !$$name->isEmpty())
                         <button
                                 x-on:click="active='{{ $name }}'"
                                 x-bind:class="{'bg-teal-700 text-teal-900': (active !== '{{ $name }}'), 'bg-teal-300 text-teal-800': (active === '{{ $name }}')}"
@@ -17,7 +17,7 @@
 
         <div class="w-full h-full">
             @foreach($tabs_collection as $name => $title)
-                @if(isset($$name) && $$name instanceof \Illuminate\Support\HtmlString && !$$name->isEmpty())
+                @if(isset($$name) && $$name instanceof $htmlable && !$$name->isEmpty())
                      <div x-cloak x-show="active === '{{ $name }}' " class="h-full">
                          {!! $$name !!}
                      </div>
