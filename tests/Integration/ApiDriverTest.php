7d7e654aae0a5e7a182c052a2d23d1fe08e004d3
sileence <sileence@users.noreply.github.com>
2024-05-05 09:13:07 +0000

    Apply php-cs-fixer changes

--- tests/Integration/ApiDriverTest.php
+++ tests/Integration/ApiDriverTest.php
@@ -63,56 +63,56 @@ class ApiDriverTest extends TestCase
         $this->assertNull(Run::first(), 'An unexpected run was added to the database when using the API driver');
         $this->assertNull(ExampleGroup::first(), 'An unexpected group was added to the database when using the API driver');
         $this->assertNull(Example::first(), 'An unexpected group was added to the database when using the API driver');
-//
-//        tap($group = $run->groups()->first(), function (ExampleGroup $exampleGroup) {
-//            $this->assertSame('Tests\Integration\ApiDriverTest', $exampleGroup->class_name);
-//            $this->assertSame('Api Driver', $exampleGroup->title);
-//            $this->assertSame('integration-api-driver', $exampleGroup->slug);
-//            $this->assertNull($exampleGroup->description);
-//        });
+        //
+        //        tap($group = $run->groups()->first(), function (ExampleGroup $exampleGroup) {
+        //            $this->assertSame('Tests\Integration\ApiDriverTest', $exampleGroup->class_name);
+        //            $this->assertSame('Api Driver', $exampleGroup->title);
+        //            $this->assertSame('integration-api-driver', $exampleGroup->slug);
+        //            $this->assertNull($exampleGroup->description);
+        //        });
 
-//        tap($group->examples()->first(), function (Example $example) use ($group) {
-//            $this->assertTrue($example->group->is($group));
-//            $this->assertSame('gets_the_list_of_users', $example->method_name);
-//            $this->assertSame('gets-the-list-of-users', $example->slug);
-//            $this->assertSame('Obtiene la lista de usuarios', $example->title);
-//            $this->assertSame('Obtiene los nombres y correos electrónicos de todos los usuarios registrados en el sistema', $example->description);
-//
-//            tap($example->requests->first(), function ($request) {
-//                $this->assertNotNull($request);
-//
-//                $this->assertSame('GET', $request->request_method);
-//                $this->assertSame('api/users', $request->request_path);
-//
-//                $this->assertSame('api/users/{status?}/{page?}', $request->route);
-//                $this->assertSame([
-//                    [
-//                        'name' => 'status',
-//                        'pattern' => '*',
-//                        'optional' => true,
-//                    ],
-//                    [
-//                        'name' => 'page',
-//                        'pattern' => '*',
-//                        'optional' => true,
-//                    ]
-//                ], $request->route_parameters);
-//
-//                $this->assertFalse($request->follows_redirect);
-//
-//                $this->assertSame([
-//                    'data' => [
-//                        [
-//                            'name' => 'Duilio Palacios',
-//                            'email' => 'duilio@example.com',
-//                        ],
-//                        [
-//                            'name' => 'Jeffer Ochoa',
-//                            'email' => 'jeff.ochoa@example.com',
-//                        ],
-//                    ]
-//                ], $request->response_body);
-//            });
-//        });
+        //        tap($group->examples()->first(), function (Example $example) use ($group) {
+        //            $this->assertTrue($example->group->is($group));
+        //            $this->assertSame('gets_the_list_of_users', $example->method_name);
+        //            $this->assertSame('gets-the-list-of-users', $example->slug);
+        //            $this->assertSame('Obtiene la lista de usuarios', $example->title);
+        //            $this->assertSame('Obtiene los nombres y correos electrónicos de todos los usuarios registrados en el sistema', $example->description);
+        //
+        //            tap($example->requests->first(), function ($request) {
+        //                $this->assertNotNull($request);
+        //
+        //                $this->assertSame('GET', $request->request_method);
+        //                $this->assertSame('api/users', $request->request_path);
+        //
+        //                $this->assertSame('api/users/{status?}/{page?}', $request->route);
+        //                $this->assertSame([
+        //                    [
+        //                        'name' => 'status',
+        //                        'pattern' => '*',
+        //                        'optional' => true,
+        //                    ],
+        //                    [
+        //                        'name' => 'page',
+        //                        'pattern' => '*',
+        //                        'optional' => true,
+        //                    ]
+        //                ], $request->route_parameters);
+        //
+        //                $this->assertFalse($request->follows_redirect);
+        //
+        //                $this->assertSame([
+        //                    'data' => [
+        //                        [
+        //                            'name' => 'Duilio Palacios',
+        //                            'email' => 'duilio@example.com',
+        //                        ],
+        //                        [
+        //                            'name' => 'Jeffer Ochoa',
+        //                            'email' => 'jeff.ochoa@example.com',
+        //                        ],
+        //                    ]
+        //                ], $request->response_body);
+        //            });
+        //        });
     }
 }
